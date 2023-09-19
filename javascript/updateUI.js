aae5f54f01e5d30844cab38aaa9324ce7b04b974
eric-armendariz <earmen3@illinois.edu>
2023-09-18 22:33:09 -0500

    fixed sidebar

--- javascript/updateUI.js
+++ javascript/updateUI.js
@@ -1,9 +1,8 @@
+
 function updateUI(route) {
-    document.addEventListener("DOMContentLoaded", function () {
-        // Get references to the text boxes and the update button
-        var routeInfo = document.getElementById("routeInfo");
-        routeInfo.textContent = "New";
-    });
+    // Get references to the text boxes and the update button
+    var routeInfo = document.getElementById("routeInfo");
+    routeInfo.textContent = "New";
     
 }
 
