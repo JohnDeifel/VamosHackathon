65c16ea3667f49ec8b61ea1af906d53a8d16b08d
eric-armendariz <earmen3@illinois.edu>
2023-09-18 21:50:41 -0500

    Added updateUI function

---
+++ javascript/updateUI.js
@@ -0,0 +1,9 @@
+function updateUI(route) {
+    document.addEventListener("DOMContentLoaded", function () {
+        // Get references to the text boxes and the update button
+        var routeInfo = document.getElementById("routeInfo");
+        routeInfo.textContent = "New";
+    });
+    
+}
+
